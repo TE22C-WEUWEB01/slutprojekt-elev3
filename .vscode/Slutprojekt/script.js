b647a9689161297d834a1ecb8eb2db33c24b762f
Karim Ryde <karye@users.noreply.github.com>
2025-04-24 14:47:54 +0200

    Initial commit

---
+++ .vscode/Slutprojekt/script.js
@@ -0,0 +1,4 @@
+document.querySelector('.button').addEventListener('click', function (){
+document.body.style.backgroundColor ="darkgrey";
+document.querySelector('html').style.backgroundColor ="darkgrey";
+})
